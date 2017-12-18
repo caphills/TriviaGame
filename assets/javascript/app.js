3b14fb59f93033474ffb8c0f53dec3cf18767fed
caphills <32250271+caphills@users.noreply.github.com>
2017-12-17 22:50:33 -0600

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,13 @@
   // $( document ).ready(function(){
 
 $('#start').on('click',function() {
-	game.start()
-// 	$('#start').remove(); 
-// 	// for loop functions
-// 	for(var i=0; i<questions.length;i++){
-// 		// append to subcontainer 
-// 		$('#subcontainer').append('<h2>'+questions[i].question+'</h2>');
-// 		// sub loop
-// 		for(var j=0;j<questions[i].answers.length;j++){
-// 			$("#subcontainer").append("<input type='radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j]);
-// 		}
-// 	}
+	game.start();
 })
+
+$(document).on('click','#end',function(){
+	game.done();
+})
+
 var questions = [{
 	question: "Before Michael takes over as \"Don\" of the family, what is Don Corleone's first name?",
 	answers: ["Carlo", "Santino", "Tito", "Vito"],
@@ -74,6 +69,76 @@ var game = {
 				$("#subcontainer").append("<input type='radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j]);
 			}
 		}
+		$('subcontainer').append('<br><button id="end">DONE</button>')
+	},
+// function to check @ of the question whether correct or not
+	done: function(){
+		$.each($("input[name='question-0']:checked"), function(){
+			if ($(this).val()===questions[0].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+
+		$.each($("input[name='question-1']:checked"), function(){
+			if ($(this).val()===questions[1].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+
+		$.each($("input[name='question-2']:checked"), function(){
+			if ($(this).val()===questions[2].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+
+		$.each($("input[name='question-3']:checked"), function(){
+			if ($(this).val()===questions[3].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+
+		$.each($("input[name='question-4']:checked"), function(){
+			if ($(this).val()===questions[4].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+
+		$.each($("input[name='question-5']:checked"), function(){
+			if ($(this).val()===questions[5].correctAnswer){
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+
+		$.each($("input[name='question-6']:checked"), function(){
+			if ($(this).val()===questions[6].correctAnswer){
+				game.correct++;
+			} 	else {
+				game.incorrect++;
+			}
+		});
+
+		this.result();
+		},
+		//print results for the game
+		result: function(){
+		clearInterval(timer);
+		//remove the time remaining
+		$('#subcontainer h2').remove();
+		$('#subcontainer').html("<h2>All done!<h2>");
+		$('#subcontainer').append("<h3>Correct Answers: "+this.correct+"</h3>");
+		$('#subcontainer').append("<h3>Incorrect Answers: "+this.incorrect+"</h3>");
+		$('#subcontainer').append("<h3>Unanswered: "+(questions.length (this.incorrect+this.correct))+"</h3>");
 	}
 }
-
